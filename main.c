e20b7cbd54af744f4f6fcabd0341c67e67848027
Deyvid9959 <deyvidoliveira35@gmail.com>
2024-12-27 00:18:03 -0300

    Validação e encerramento da partida apos vitoria, também foram realizados algum testes.

--- main.c
+++ main.c
@@ -9,39 +9,44 @@ int tabuleiro[TAM][TAM];
 int linhas = TAM, colunas = TAM;
 int linhaVazia, colunaVazia; // Para rastrear a posição do espaço vazio
 
-
-void verificarVitoria(int tabuleiro[linhas][colunas]){
-    int count =1, verif =0;
-
-    for (int i = 0; i<linhas; i++) {
-        for (int j = 0; j<colunas; j++) {
-            if (tabuleiro[i][j]==count) {
+int verificarVitoria(int tabuleiro[linhas][colunas])
+{
+    int count = 1, verif = 0;
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            if (tabuleiro[i][j] == count)
+            {
                 verif = verif + 1;
-
             }
-            count = count+1;
-
+            count = count + 1;
         }
     }
-    if (verif==15) {
+    if (verif == 15)
+    {
         printf("Voce venceu");
+        menu();
     }
-    //so pra verificar se funciona
-    else {
-        printf("deu bom nao");
-    }
+    
 }
 
 // Inicializar o tabuleiro com números de 1 a 15 e espaço vazio
-void inicializarTabuleiro() {
+void inicializarTabuleiro()
+{
     int valor = 1;
-    for (int i = 0; i < TAM; i++) {
-        for (int j = 0; j < TAM; j++) {
-            if (valor < TAM * TAM) {
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            if (valor < TAM * TAM)
+            {
                 tabuleiro[i][j] = valor++;
-            } else {
+            }
+            else
+            {
                 tabuleiro[i][j] = 0; // Espaço vazio
-                linhaVazia = i; // Salvar posição inicial do espaço vazio
+                linhaVazia = i;      // Salvar posição inicial do espaço vazio
                 colunaVazia = j;
             }
         }
@@ -49,14 +54,20 @@ void inicializarTabuleiro() {
 }
 
 // Função para exibir o tabuleiro
-void exibirTabuleiro() {
+void exibirTabuleiro()
+{
     printf("\nTabuleiro:\n\n");
-    for (int i = 0; i < TAM; i++) {
+    for (int i = 0; i < TAM; i++)
+    {
         printf("+----+----+----+----+\n");
-        for (int j = 0; j < TAM; j++) {
-            if (tabuleiro[i][j] == 0) {
+        for (int j = 0; j < TAM; j++)
+        {
+            if (tabuleiro[i][j] == 0)
+            {
                 printf("|    ");
-            } else {
+            }
+            else
+            {
                 printf("| %2d ", tabuleiro[i][j]);
             }
         }
@@ -66,9 +77,11 @@ void exibirTabuleiro() {
 }
 
 // Embaralhar o tabuleiro
-void embaralharPeca() {
+void embaralharPeca()
+{
     srand(time(NULL));
-    for (int i = 0; i < 1000; i++) { // Realizar múltiplas trocas para um bom embaralhamento
+    for (int i = 0; i < 1000; i++)
+    { // Realizar múltiplas trocas para um bom embaralhamento
         int x1 = rand() % TAM, y1 = rand() % TAM;
         int x2 = rand() % TAM, y2 = rand() % TAM;
         int temp = tabuleiro[x1][y1];
@@ -76,10 +89,13 @@ void embaralharPeca() {
         tabuleiro[x2][y2] = temp;
 
         // Atualizar posição do espaço vazio se for trocado
-        if (tabuleiro[x1][y1] == 0) {
+        if (tabuleiro[x1][y1] == 0)
+        {
             linhaVazia = x1;
             colunaVazia = y1;
-        } else if (tabuleiro[x2][y2] == 0) {
+        }
+        else if (tabuleiro[x2][y2] == 0)
+        {
             linhaVazia = x2;
             colunaVazia = y2;
         }
@@ -87,7 +103,9 @@ void embaralharPeca() {
 }
 
 // Exibir regras do jogo
-void exibirRegras() {
+void exibirRegras()
+{
+    
     printf("\nRegras do Jogo dos 15:\n");
     printf("1. O tabuleiro é composto por números de 1 a 15 e um espaço vazio.\n");
     printf("2. O objetivo é organizar os números em ordem crescente, deixando o espaço vazio no final.\n");
@@ -98,46 +116,60 @@ void exibirRegras() {
 }
 
 // Movimentar espaço vazio com WASD
-void movimentarEspaco(char direcao) {
+void movimentarEspaco(char direcao)
+{
     int novaLinha = linhaVazia, novaColuna = colunaVazia;
 
-    if (direcao == 'w' || direcao == 'W') {
+    if (direcao == 'w' || direcao == 'W')
+    {
         novaLinha--;
-    } else if (direcao == 'a' || direcao == 'A') {
+    }
+    else if (direcao == 'a' || direcao == 'A')
+    {
         novaColuna--;
-    } else if (direcao == 's' || direcao == 'S') {
+    }
+    else if (direcao == 's' || direcao == 'S')
+    {
         novaLinha++;
-    } else if (direcao == 'd' || direcao == 'D') {
+    }
+    else if (direcao == 'd' || direcao == 'D')
+    {
         novaColuna++;
     }
 
     // Verificar se o movimento é válido
-    if (novaLinha >= 0 && novaLinha < TAM && novaColuna >= 0 && novaColuna < TAM) {
+    if (novaLinha >= 0 && novaLinha < TAM && novaColuna >= 0 && novaColuna < TAM)
+    {
         tabuleiro[linhaVazia][colunaVazia] = tabuleiro[novaLinha][novaColuna];
         tabuleiro[novaLinha][novaColuna] = 0;
         linhaVazia = novaLinha;
         colunaVazia = novaColuna;
 
-        //aqui
+        // aqui
         verificarVitoria(tabuleiro);
-    } else {
+    }
+    else
+    {
         printf("Movimento inválido!\n");
     }
 }
 
 // Jogar o jogo
-void jogar() {
+void jogar()
+{
     inicializarTabuleiro();
     embaralharPeca();
     char movimento;
 
-    do {
+    do
+    {
         exibirTabuleiro();
         printf("\nUse W, A, S, D para mover o espaço vazio. Pressione Q para sair.\n");
         printf("Movimento: ");
         scanf(" %c", &movimento);
 
-        if (movimento == 'q' || movimento == 'Q') {
+        if (movimento == 'q' || movimento == 'Q')
+        {
             printf("\nVocê saiu do jogo.\n");
             break;
         }
@@ -146,11 +178,12 @@ void jogar() {
     } while (1);
 }
 
-
 // Menu principal
-void menu() {
+void menu()
+{
     char opcao;
-    do {
+    do
+    {
         printf("\nBem-vindo ao Jogo dos 15!\n\n");
         printf("A. Jogar\n");
         printf("B. Regras do jogo\n");
@@ -158,7 +191,8 @@ void menu() {
         printf("\nOpção: ");
         scanf(" %c", &opcao);
 
-        switch (opcao) {
+        switch (opcao)
+        {
         case 'a':
         case 'A':
             jogar();
@@ -182,9 +216,8 @@ void menu() {
     } while (opcao != 'c' && opcao != 'C');
 }
 
-
-
-int main() {
+int main()
+{
     menu();
     return 0;
 }
