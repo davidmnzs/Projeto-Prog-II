130cb1ea45c68a3be0edf92add88cf213a4c8460
David Menezes <132839452+davidmnzs@users.noreply.github.com>
2025-01-23 20:37:32 -0300

    includes em 1 so pasta

---
+++ arquivos/meu_include.h
@@ -0,0 +1,22 @@
+#ifndef MEU_INCLUDE_H
+#define MEU_INCLUDE_H
+
+// Bibliotecas SDL2
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+
+// Bibliotecas padrão C
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+// Biblioteca C++
+#include <vector>
+
+#endif // MEU_INCLUDE_H
